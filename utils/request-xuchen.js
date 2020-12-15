b74c3f2df17b6c129a60905d9c06993cd1b49fcb
xuchen-xu <164919752@qq.com>
2020-12-15 09:33:32 +0800

    上传自己的request测试

--- utils/request-xuchen.js
+++ utils/request-xuchen.js
@@ -17,26 +17,4 @@ export function myRequestGet(url, data) {
 }
 
 
-export function myRequestPost(url, data) {
-	return new Promise((resolve, reject) => {
-		uni.request({
-			//#ifdef H5
-			url: url,
-			//#endif
-			//#ifdef MP-WEIXIN | MP-ALIPAY
-			url: baseUrl + url,
-			//#endif
-			header: {
-				'Content-Type': 'application/json'
-			},
-			method: "POST",
-			data: data,
-			success: function(res) {
-				resolve(res.data)
-			},
-			fail: function(err) {
-				reject(err)
-			}
-		})
-	})
-}
+
