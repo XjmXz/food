cb4e85e86c7af6297256e4cd6b23c4967ed745ae
xuchen-xu <164919752@qq.com>
2020-12-15 09:16:06 +0800

    爬取数据和头部组件再次上传

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script>
+	import './static/iconfont-weapp-icon.css';
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	page {
+		height: 100%;
+	}
+</style>
---
+++ pages/square/square.vue
@@ -0,0 +1,72 @@
+<template>
+	<view>
+		<uni-segmented-control :current="current" :values="items" @clickItem="onClickItem" style-type="button" active-color="#4cd964"></uni-segmented-control>
+		<view class="content">
+			<scroll-view v-if="current === 0" scroll-y>
+				选项卡1的内容
+			</scroll-view>
+			<view v-if="current === 1">
+				选项卡2的内容
+			</view>
+			<view v-if="current === 2">
+				选项卡3的内容
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniSegmentedControl from "@/components/uni/uni-segmented-control/uni-segmented-control.vue"
+	import {
+		myRequestGet
+	} from '@/utils/request-xuchen.js';
+	export default {
+		data() {
+			return {
+				id: "",
+				square: [],
+				items: ['看帖', '投票', '精华'],
+				current: 0
+			};
+		},
+		onLoad(options) {
+			this.id = options.id;
+			this.getSquareWatch()
+		},
+		components: {
+			uniSegmentedControl
+		},
+		methods: {
+			//https: //api5.meishichina.com/api.php?p=%7B%22m%22%3A%7B%22pai_getPaiList%22%3A%7B%22type%22%3A%22vote%22%2C%22
+			// pageindex%22%3A1%7D%7D%2C%22openudid%22%3A%22meishichina%22%2C%22uid%22%3A12656254%2C%22appver%22%3A%223028%22%2C%22device%22%3A%22microsoftmicrosoft%22%2C%22appname%22%3A%22xcx_weixin%22%2C%22session%22%3A%22xcx_weixin%3Aweixin%3A12656254%3Ag4j5SkNMeV2KNerCulQ1YPuLTnGKQI1J%22%7D			
+			async getSquareWatch() {
+				let result = await myRequestGet('/api.php', {
+					p: {
+						"m": {
+							"pai_getPaiList": {
+								"type": "all",
+								"pageindex": 2
+							}
+						},
+						"openudid": "meishichina",
+						"uid": 12656254,
+						"appver": "3028",
+						"device": "microsoftmicrosoft",
+						"appname": "xcx_weixin",
+						"session": "xcx_weixin:weixin:12656254:g4j5SkNMeV2KNerCulQ1YPuLTnGKQI1J"
+					}
+				});
+				this.square = result.pai_getPaiList.data;
+			},
+			onClickItem(e) {
+				if (this.current !== e.currentIndex) {
+					this.current = e.currentIndex;
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
